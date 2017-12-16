89337ac03c34e01c1061b544dfe4666e1913c730
Jivka Ignatova <ignatova.jivka@gmail.com>
2017-12-16 18:10:31 +0200

    test

--- VetClinic/Startup.cs
+++ VetClinic/Startup.cs
@@ -1,9 +1,13 @@
-﻿namespace VetClinic
+﻿using VetClinic.Data.Models;
+
+namespace VetClinic
 {
     public class Startup
     {
         static void Main()
         {
+            var pesho = new Owner("Pesho", "Peshev", "09678676");
+            pesho.PrintPets();
         }
     }
 }
