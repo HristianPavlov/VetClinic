e638fdbaaed7213206468cb64f27006441f0431f
mkovachev <martin.kovachev@outlook.com>
2017-12-24 23:58:19 +0200

    trying to imitate a db

---
+++ VetClinic.Data/Contracts/IPetsOwnerRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace VetClinic.Data.Contracts
+{
+    public interface IPetsOwnerRepository
+    {
+        ICollection<IPetOwner> PetsOwners { get; }
+
+        IPetOwner GetById(string ownerId);
+
+        void AddOwner(IPetOwner petOwner);
+
+        void RemoveOwner(string id);
+    }
+}
