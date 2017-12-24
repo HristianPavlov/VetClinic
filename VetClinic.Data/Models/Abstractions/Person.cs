128599d7ffc6d51dde4d92ecb4130ff4b48d804f
mkovachev <martin.kovachev@outlook.com>
2017-12-24 22:44:34 +0200

    class props updated

---
+++ VetClinic.Data/Models/Abstractions/Person.cs
@@ -0,0 +1,42 @@
+﻿namespace VetClinic.Data.Models.Abstractions
+{
+    using Bytes2you.Validation;
+    using VetClinic.Data.Common;
+    using VetClinic.Data.Contracts;
+
+    public abstract class Person: IPerson
+    {
+        private readonly string id;
+        private readonly string firstName;
+        private readonly string lastName;
+        private readonly string phoneNumber;
+        private readonly string email;
+
+        public Person(string firstName, string lastName, string phoneNumber, string email)
+        {
+            Guard.WhenArgument(firstName, "First name is null or empty").IsNullOrEmpty().Throw();
+            Guard.WhenArgument(firstName.Length, "First name has invalid length").IsLessThan(3).IsGreaterThan(15).Throw();
+            Guard.WhenArgument(lastName, "First name is null or empty").IsNullOrEmpty().Throw();
+            Guard.WhenArgument(lastName.Length, "First name has invalid length").IsLessThan(3).IsGreaterThan(15).Throw();
+            Guard.WhenArgument(phoneNumber, "First name is null or empty").IsNullOrEmpty().Throw();
+            Guard.WhenArgument(phoneNumber.Length, "First name has invalid length").IsLessThan(3).IsGreaterThan(15).Throw();
+
+            id = IdGenerator.GenerateId(typeof(IPerson));
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
+
+        public string FirstName => this.firstName;
+
+        public string LastName => this.lastName;
+
+        public string PhoneNumber => this.phoneNumber;
+
+        public string Id => this.Id;
+
+        public string Email => this.Email;
+
+    }
+}
