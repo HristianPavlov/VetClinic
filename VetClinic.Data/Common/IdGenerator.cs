078b22b9e73e442bf577a782c11964332d3011d5
mkovachev <martin.kovachev@outlook.com>
2017-12-24 22:15:54 +0200

    extracted class generate Id with prefix extension possible for all future classes

---
+++ VetClinic.Data/Common/IdGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using VetClinic.Data.Contracts;
+
+namespace VetClinic.Data.Common
+{
+    public static class IdGenerator
+    {
+        public static string GenerateId(object classType)
+        {
+            var sb = new StringBuilder();
+            var id = Guid.NewGuid().ToString();
+
+            switch (classType)
+            {
+                case Type person when person == typeof(IPerson): sb.Append("P"); break;
+                case Type animal when animal == typeof(IAnimal): sb.Append("A"); break;
+                default: throw new ArgumentException("No such type found");
+            }
+
+            sb.Append(id);
+
+            return sb.ToString();
+        }
+}
+}
