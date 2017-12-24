0087e7c7a79cc12e5c026358f775f7c38f40b2f5
mkovachev <martin.kovachev@outlook.com>
2017-12-24 22:27:00 +0200

    refactor clinic staff person class

---
+++ VetClinic.Data/Models/ClinicStaffPerson.cs
@@ -0,0 +1,18 @@
+﻿namespace VetClinic.Data.Models
+{
+    using Abstractions;
+    using VetClinic.Data.Common.Enums;
+
+    public class ClinicStaffPerson : Person
+    {
+        private readonly RoleType role;
+
+        public ClinicStaffPerson(string firstName, string lastName, string phoneNumber, string email, RoleType role)
+            : base(firstName, lastName, phoneNumber, email)
+        {
+            this.role = role;
+        }
+
+        public RoleType Role => this.role;
+    }
+}
