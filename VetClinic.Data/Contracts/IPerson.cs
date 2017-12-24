94b09d0e199ed82ab67856fd9d71e9e61b0f5cb2
mkovachev <martin.kovachev@outlook.com>
2017-12-24 22:02:05 +0200

    interface for person added

---
+++ VetClinic.Data/Contracts/IPerson.cs
@@ -0,0 +1,15 @@
+﻿namespace VetClinic.Data.Contracts
+{
+    public interface IPerson
+    {
+        string Id { get; }
+
+        string FirstName { get; }
+
+        string LastName { get; }
+
+        string PhoneNumber { get; }
+
+        string Email { get; }
+    }
+}
