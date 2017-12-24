46b4a00a710d94a4017075ffc4a0de92c0d27c69
mkovachev <martin.kovachev@outlook.com>
2017-12-25 00:23:21 +0200

    refactor

---
+++ VetClinic.Core/ClinicServices/Implementations/ClinicServicesListing.cs
@@ -0,0 +1,62 @@
+﻿namespace VetClinic.Core.ClinicServices.Implementations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using VetClinic.Core.ClinicServices.Contracts;
+
+    public class ClinicServicesListing: IClinicServicesListing
+    {
+        private readonly ICollection<IService> services;
+    
+        public ClinicServicesListing()
+        {
+            this.services = new List<IService>();
+        }
+
+        public ICollection<IService> Services => new List<IService>();
+
+        public void AddServices(IService service)
+        {
+            if (service == null)
+            {
+                throw new Exception("No such service found");
+            }
+
+            this.services.Add(service);
+        }
+
+        public void RemoveServices(IService service)
+        {
+            if (service == null)
+            {
+                throw new Exception("No such service found");
+            }
+
+            this.services.Remove(service);
+        }
+
+        public bool ContainsService(string id)
+        {
+            return this.services.Any(s => s.Id == id);
+        }
+
+        public void FindById(string id)
+        {
+            this.services.FirstOrDefault(s => s.Id == id);
+        }
+
+        public string ListAllServices()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var service in this.services)
+            {
+                sb.AppendLine($"{service.Print()}");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
