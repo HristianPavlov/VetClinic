0e209c8bf2e2863329d84d8575f1784864c3410a
mkovachev <martin.kovachev@outlook.com>
2017-12-25 18:32:12 +0200

    interface implemented for command repository

---
+++ VetClinic/Commands/Repositories/ICommandRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace VetClinic.Commands.Repositories
+{
+    using System.Collections.Generic;
+    using VetClinic.Commands.Contracts;
+
+    public interface ICommandRepository
+    {
+        ICollection<ICommandExample> Commands { get; }
+
+        ICommandExample GetByName(string name);
+
+        void AddComamnd(ICommandExample command);
+
+        void RemoveCommand(string name);
+    }
+}
