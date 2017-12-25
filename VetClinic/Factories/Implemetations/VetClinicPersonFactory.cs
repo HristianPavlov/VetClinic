4af9f089a68ceeb1527d5e7b54d4293b198554cb
mkovachev <martin.kovachev@outlook.com>
2017-12-25 08:58:11 +0200

    person factory implemented

---
+++ VetClinic/Factories/Implemetations/VetClinicPersonFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace VetClinic.Factories.Implemetations
+{
+    using VetClinic.Data.Common.Enums;
+    using VetClinic.Data.Contracts;
+    using VetClinic.Data.Models;
+    using VetClinic.Factories.Contracts;
+
+    public class VetClinicPersonFactory : IVetClinicPersonFactory
+    {
+        public IPerson CreatePetOwner(string firstName, string lastName, string phoneNumber, string email, decimal wallet) => new PetOwner(firstName, lastName, phoneNumber, email);
+
+        public IPerson CreateClinicStaffPerson(string firstName, string lastName, string phoneNumber, string email, RoleType role)
+            => new ClinicStaffPerson(firstName, lastName, phoneNumber, email, role);
+    }
+}
