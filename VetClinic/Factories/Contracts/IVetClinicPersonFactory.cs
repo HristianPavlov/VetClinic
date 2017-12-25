8e97efc4293299a2430e293ed3833405acc5a49e
mkovachev <martin.kovachev@outlook.com>
2017-12-25 09:20:05 +0200

    person factory implemented

---
+++ VetClinic/Factories/Contracts/IVetClinicPersonFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace VetClinic.Factories.Contracts
+{
+    using VetClinic.Data.Common.Enums;
+    using VetClinic.Data.Contracts;
+
+    public interface IVetClinicPersonFactory
+    {
+        IPerson CreatePetOwner(string firstName, string lastName, string phoneNumber, string email, decimal wallet);
+
+        IPerson CreateClinicStaffPerson(string firstName, string lastName, string phoneNumber, string email, RoleType role);
+    }
+}
