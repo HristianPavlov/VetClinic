96eae04a9ff17633082f1b18af01d38dd888c726
mkovachev <martin.kovachev@outlook.com>
2017-12-25 18:38:58 +0200

    command repo implemented

---
+++ VetClinic/Commands/Repositories/CommandRepository.cs
@@ -0,0 +1,33 @@
+﻿namespace VetClinic.Commands.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using VetClinic.Commands.Contracts;
+
+    public class CommandRepository : ICommandRepository
+    {
+        private readonly ICollection<ICommandExample> commands;
+
+        public CommandRepository()
+        {
+            this.commands = new List<ICommandExample>();
+        }
+
+        public ICollection<ICommandExample> Commands => new List<ICommandExample>(this.commands);
+
+        public void AddComamnd(ICommandExample command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICommandExample GetByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveCommand(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
