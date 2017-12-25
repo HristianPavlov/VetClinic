cfc349fabeaed54a3d84bfc75a49fc5541b03f0a
mkovachev <martin.kovachev@outlook.com>
2017-12-25 15:53:33 +0200

    refactor to more generic class

---
+++ VetClinic.Data/Repositories/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using VetClinic.Data.Contracts;
+
+namespace VetClinic.Data.Repositories
+{
+    public interface IUserRepository
+    {
+        ICollection<IUser> Users { get; }
+
+        IUser GetById(string id);
+
+        void AddUser(IUser user);
+
+        void RemoveUser(string id);
+    }
+}
