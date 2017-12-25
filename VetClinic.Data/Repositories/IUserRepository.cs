ca7cf78cb3576ff29b8aaffa2ab221d121a252c6
mkovachev <martin.kovachev@outlook.com>
2017-12-25 17:31:35 +0200

    updated

--- VetClinic.Data/Repositories/IUserRepository.cs
+++ VetClinic.Data/Repositories/IUserRepository.cs
@@ -12,5 +12,13 @@ namespace VetClinic.Data.Repositories
         void AddUser(IUser user);
 
         void RemoveUser(string id);
+
+        void CreateCat(IUser user, ICat cat);
+
+        void CreateDog(IUser user, IDog dog);
+
+        void CreateHamster(IUser user, IHamster hamster);
+
+        void RemoveAnimal(string id);
     }
 }
