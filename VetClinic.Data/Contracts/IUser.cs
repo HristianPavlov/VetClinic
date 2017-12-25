81e6a0cd34076fc759e0b01b417392d2386e81ca
mkovachev <martin.kovachev@outlook.com>
2017-12-25 15:52:54 +0200

    refactor to more generic class

---
+++ VetClinic.Data/Contracts/IUser.cs
@@ -0,0 +1,20 @@
+﻿namespace VetClinic.Data.Contracts
+{
+    using System.Collections.Generic;
+
+    public interface IUser : IPerson
+    {
+        ICollection<IAnimal> Pets { get; }
+
+        void AddPet(IAnimal pet);
+
+        void RemovePet(IAnimal pet);
+
+        void PayForServices(decimal cost);
+
+        void BuyMedicine(decimal cost);
+
+        void ListAllPets();
+
+    }
+}
