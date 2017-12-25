109b8b95f7b846d6a9557427ecdc0b89ab0832bb
mkovachev <martin.kovachev@outlook.com>
2017-12-25 18:25:07 +0200

    possible solution for ICommand

---
+++ VetClinic/Commands/Contracts/ICommandExample.cs
@@ -0,0 +1,15 @@
+﻿namespace VetClinic.Commands.Contracts
+{
+    using System.Collections.Generic;
+
+    public interface ICommandExample
+    {
+        void CreateUser(IList<string> parameters);
+
+        void RemoveUser(IList<string> parameters);
+
+        void GetUserPets(IList<string> parameters);
+
+        // add all other commands here
+    }
+}
