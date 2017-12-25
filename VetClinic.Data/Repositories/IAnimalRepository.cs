0f5142abf89f176060bd5bdaf0507a0bfb11ef33
mkovachev <martin.kovachev@outlook.com>
2017-12-25 17:49:36 +0200

    interface added

---
+++ VetClinic.Data/Repositories/IAnimalRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace VetClinic.Data.Repositories
+{
+    using System.Collections.Generic;
+    using VetClinic.Data.Contracts;
+
+    public interface IAnimalRepository
+    {
+        IDictionary<IUser, IAnimal> Animals { get; }
+
+        IAnimal GetById(string id);
+
+        void CreateCat(IUser user, ICat cat);
+
+        void CreateDog(IUser user, IDog dog);
+
+        void CreateHamster(IUser user, IHamster hamster);
+
+        void RemoveAnimal(string id);
+    }
+}
