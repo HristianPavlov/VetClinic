45c1f8c5c3467d4dcec5b9a322ae9cbf2b2caa72
mkovachev <martin.kovachev@outlook.com>
2017-12-26 14:13:00 +0200

    VetClinic/EngineExample.cs

--- VetClinic/Factories/Implemetations/AnimalFactory.cs
+++ VetClinic/Factories/Implemetations/AnimalFactory.cs
@@ -1,5 +1,7 @@
 ﻿namespace VetClinic.Factories.Implemetations
 {
+    using System;
+    using System.Linq;
     using VetClinic.Data.Common.Enums;
     using VetClinic.Data.Contracts;
     using VetClinic.Data.Models;
@@ -15,5 +17,17 @@
 
         public IAnimal CreateHammster(string name, AnimalGenderType gender, int age)
             => new Hamster(name, gender, age);
+
+        public IAnimal CreateAnimal(string name, AnimalGenderType gender, string breed, int age)
+        {
+            var animalTypes = Enum.GetValues(typeof(AnimalType)).Cast<AnimalType>().ToList();
+            switch (animalTypes.ToString())
+            {
+                case "Dog": return new Dog(name, gender, breed, age);
+                case "Cat": return new Cat(name, gender, age);
+                case "Hamster": return new Hamster(name, gender, age);
+                default: throw new ArgumentException("no such type of animal can be services");
+            }
+        }
     }
 }
