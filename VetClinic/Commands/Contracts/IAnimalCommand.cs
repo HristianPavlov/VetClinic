85309e052352bd228205a3d1ce3fcb23068c92ae
mkovachev <martin.kovachev@outlook.com>
2017-12-26 14:14:25 +0200

    new commands added

---
+++ VetClinic/Commands/Contracts/IAnimalCommand.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace VetClinic.Commands.Contracts
+{
+    public interface IAnimalCommand
+    {
+        void CreateAnimal(IList<string> parameters);
+
+        void RemoveAnimal(IList<string> parameters);
+
+        string ListAllPets();
+    }
+}
