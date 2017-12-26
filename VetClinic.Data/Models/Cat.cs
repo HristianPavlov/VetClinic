cdc5f5024f95d04eeb1bf5945c1394e4a5d3a313
mkovachev <martin.kovachev@outlook.com>
2017-12-26 14:10:16 +0200

    refactor

--- VetClinic.Data/Models/Cat.cs
+++ VetClinic.Data/Models/Cat.cs
@@ -18,11 +18,11 @@
         {
             StringBuilder str = new StringBuilder();
 
+            str.AppendLine($"Id: {this.Id}");
             str.AppendLine($"Pet Type: {this.Type}");
             str.AppendLine($"Name: {this.Name}");
             str.AppendLine($"Gender: {this.Gender}");
             str.AppendLine($"Age: {this.Age}");
-            str.AppendLine($"Id: {this.Id}");
 
             return str.ToString();
 
