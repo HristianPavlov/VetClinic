62559d491696f4c3d74f150a26e0dfae5d2c9adb
mkovachev <martin.kovachev@outlook.com>
2017-12-26 14:12:47 +0200

    VetClinic/EngineExample.cs

--- VetClinic/Factories/Contracts/IAnimalFactory.cs
+++ VetClinic/Factories/Contracts/IAnimalFactory.cs
@@ -5,6 +5,8 @@
 
     public interface IAnimalFactory
     {
+        IAnimal CreateAnimal(string name, AnimalGenderType gender, string breed, int age);
+
         IAnimal CreateDog(string name, AnimalGenderType gender, string breed, int age);
 
         IAnimal CreateCat(string name, AnimalGenderType gender, int age);
