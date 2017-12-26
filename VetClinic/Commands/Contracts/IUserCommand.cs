8bf1b6b773b4dbe0056ec10178c7257a28a0a647
mkovachev <martin.kovachev@outlook.com>
2017-12-26 11:37:38 +0200

    refactor

---
+++ VetClinic/Commands/Contracts/IUserCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace VetClinic.Commands.Contracts
+{
+    using System.Collections.Generic;
+
+    public interface ICommandExample
+    {
+        void CreateUser(IList<string> parameters);
+
+        void RemoveUser(IList<string> parameters);
+
+        void GetUserPets(IList<string> parameters);
+    }
+}
