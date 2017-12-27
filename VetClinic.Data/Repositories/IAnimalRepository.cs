d13b4132bde14830fbcc9751bcffc4f0cd2dde63
mkovachev <martin.kovachev@outlook.com>
2017-12-27 13:31:50 +0200

    refactor

--- VetClinic.Data/Repositories/IAnimalRepository.cs
+++ VetClinic.Data/Repositories/IAnimalRepository.cs
@@ -7,8 +7,6 @@
     {
         ICollection<IAnimal> Animals { get; }
 
-        IAnimal GetById(string id);
-
         void AddAnimal(string userId, IAnimal animal);
 
         void RemoveAnimal(string id);
