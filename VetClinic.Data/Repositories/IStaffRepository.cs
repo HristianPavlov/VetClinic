d39cddb321caf3d37880453ef0032574d5947ef0
mkovachev <martin.kovachev@outlook.com>
2017-12-27 10:58:47 +0200

    staff repo interface implemented

---
+++ VetClinic.Data/Repositories/IStaffRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using VetClinic.Data.Contracts;
+
+namespace VetClinic.Data.Repositories
+{
+    public interface IStaffRepository
+    {
+        ICollection<IStaffPerson> Staff { get; }
+
+        void AddStaffPerson(IStaffPerson user);
+
+        void RemoveStaffPerson(string id);
+    }
+}
