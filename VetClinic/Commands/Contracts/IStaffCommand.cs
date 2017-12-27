167670fdb8c61eba41d1dffea53c39187b507523
mkovachev <martin.kovachev@outlook.com>
2017-12-27 11:05:35 +0200

    interface for staff commands added

---
+++ VetClinic/Commands/Contracts/IStaffCommand.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace VetClinic.Commands.Contracts
+{
+    public interface IStaffCommand
+    {
+        void CreateStaffPerson(IList<string> parameters);
+
+        void RemoveStaffPerson(IList<string> parameters);
+      
+        void ListStaff();
+    }
+}
