ab144962cff76aeec63f3a1812830025c2e6af24
mkovachev <martin.kovachev@outlook.com>
2017-12-27 13:33:27 +0200

    new dependencies added

--- VetClinic/Startup.cs
+++ VetClinic/Startup.cs
@@ -8,38 +8,20 @@
     {
         static void Main()
         {
-            //var c = new Cat("asss", AnimalGenderType.male, -1);
-            //Console.WriteLine(c.Id);
-
-            //var pesho = new Owner("Pesho", "Peshev", "09678676");
-            //pesho.PrintPets();
-            //Animal kat = new Dog("adas",GenderType.Male,37,"Ulichna");
-            //pesho.AddPet(kat);
-            //Animal a = new Hamster("adas",GenderType.Male);
-            //pesho.AddPet(a);
-
-            //Animal ww = new Cat("adas",GenderType.Male,37);
-            //pesho.AddPet(ww);
-            //pesho.PrintPets();
-
-            //var clinicServices = new ClinicServices();
-            //clinicServices.AddServices(new Service("Surgery", 1000));
-            //clinicServices.AddServices(new Service("Vaccination", 50));
-            //System.Console.WriteLine(clinicServices.ListAllServices());
-
-            //Engine.Instance.Start();
             var personFactory = new PersonFactory();
             var animalFactory = new AnimalFactory();
 
             var userDb = new UserRepository();
+            var employeeDb = new EmployeeRepository();
             var animalDb = new AnimalRepository(userDb);
 
             var userCommands = new UserCommand(personFactory, userDb);
             var animalCommands = new AnimalCommand(animalFactory, animalDb);
+            var employeeCommands = new EmployeeCommand(personFactory, employeeDb);
 
-            var engine = new Engine(userDb, userCommands, animalCommands);
+            var engine = new Engine(userCommands, animalCommands, employeeCommands);
+
             engine.Start();
-
         }
     }
 }
