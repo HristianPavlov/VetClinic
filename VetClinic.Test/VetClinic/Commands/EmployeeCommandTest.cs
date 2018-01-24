39e18fe993177bf662c4fe616f6653644c0e2166
mkovachev <martin.kovachev@outlook.com>
2018-01-24 19:46:08 +0200

    personFactory fully tested employeeCommand constructor tested with mocked dependecies

---
+++ VetClinic.Test/VetClinic/Commands/EmployeeCommandTest.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using VetClinic.Commands.Implementations;
+using VetClinic.Common.ConsoleServices.Contracts;
+using VetClinic.Data.Enums;
+using VetClinic.Data.Models;
+using VetClinic.Data.Repositories.Contracts;
+using VetClinic.Factories.Contracts;
+
+namespace VetClinic.Test.VetClinic.Commands
+{
+    [TestClass]
+    public class EmployeeCommandTest
+    {
+        // Constructor
+        [TestMethod]
+        public void Constructor_Should_Return_New_Instance_Of_Class_EmployeeCommand()
+        {
+            // Arrange
+            var personFactoryMock = new Mock<IPersonFactory>();
+            var employeesRepoMock = new Mock<IEmployeeRepository>();
+            var writerMock = new Mock<IWriter>();
+
+            // Act
+            var employeeCommand = new EmployeeCommand(personFactoryMock.Object, employeesRepoMock.Object, writerMock.Object);
+
+            // Assert
+            Assert.IsInstanceOfType(employeeCommand, typeof(EmployeeCommand));
+        }
+
+        // Methods
+        [TestMethod]
+        public void CreateEmployee_Should_Return_Correctly_FirstName()
+        {
+            // Arrange
+            var personFactoryMock = new Mock<IPersonFactory>();
+            var employeesRepoMock = new Mock<IEmployeeRepository>();
+            var writerMock = new Mock<IWriter>();
+
+            // Act
+            var employeeCommand = new EmployeeCommand(personFactoryMock.Object, employeesRepoMock.Object, writerMock.Object);
+
+            var args = new List<string>() {
+                "firstname",
+                "lastname",
+                "phone",
+                "email",
+                "admin"
+            };
+
+            employeeCommand.CreateEmployee(args);
+
+            // Assert
+            // Assert.IsInstanceOfType(employeeCommand.CreateEmployee(args), typeof(Employee));
+        }
+    }
+}
---
+++ VetClinic.Test/VetClinic/Factories/PersonFactoryTest.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VetClinic.Data.Enums;
+using VetClinic.Data.Models;
+using VetClinic.Factories.Implemetations;
+
+namespace VetClinic.Test.VetClinic.Factories
+{
+    [TestClass]
+    public class PersonFactoryTest
+    {
+        [TestMethod]
+        public void Constructor_Should_Create_New_Instance_Of_Factory_Class()
+        {
+            // Arrange & Act
+            var personFactory = new PersonFactory();
+
+            // Assert
+            Assert.IsInstanceOfType(personFactory, typeof(PersonFactory));
+        }
+
+        [TestMethod]
+        public void _Should_Create_New_Instance_Of_User_Class()
+        {
+            // Arrange
+            var personFactory = new PersonFactory();
+
+            // Act
+            var user = personFactory.CreateUser("firstname", "lastname", "phone", "email");
+
+            // Assert
+            Assert.IsInstanceOfType(user, typeof(User));
+        }
+
+        [TestMethod]
+        public void _Should_Create_New_Instance_Of_Employee_Class()
+        {
+            // Arrange
+            var personFactory = new PersonFactory();
+
+            // Act
+            var employee = personFactory.CreateEmployee("firstname", "lastname", "phone", "email", RoleType.admin);
+
+            // Assert
+            Assert.IsInstanceOfType(employee, typeof(Employee));
+        }
+    }
+}
