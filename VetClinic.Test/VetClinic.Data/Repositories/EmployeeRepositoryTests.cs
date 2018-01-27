e8c4dd28c67a042d1ea487a16e34a4517bcd61b2
mkovachev <martin.kovachev@outlook.com>
2018-01-27 11:59:16 +0200

    EmployeeRepository constructor tested

---
+++ VetClinic.Test/VetClinic.Data/Repositories/EmployeeRepositoryTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VetClinic.Data.Repositories.Implementations;
+
+namespace VetClinic.Test.VetClinic.Data.Repositories
+{
+    [TestClass]
+    public class EmployeeRepositoryTests
+    {
+        [TestMethod]
+        public void Constructor_Should_Return_New_Instance()
+        {
+            // Arrange & Act
+            var employeeRepository = new EmployeeRepository();
+
+            // Assert
+            Assert.IsInstanceOfType(employeeRepository, typeof(EmployeeRepository));
+        }
+
+        [TestMethod]
+        public void Constructor_Should_Initialize_EmployeeList()
+        {
+            // Arrange & Act
+            var employeeRepository = new EmployeeRepository();
+
+            // Assert
+            Assert.IsNotNull(employeeRepository.Employees);
+        }
+    }
+}
